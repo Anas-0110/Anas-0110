efd9787a2dc6066458073c50259b5fa53419d9b6
Anas <B122310139@student.utem.edu.my>
2024-12-15 11:21:29 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Anas_Ikhwan:XGZgn0hYSUipQCAO@cluster0.jgb94.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://Anas_Ikhwan:<db_password>@cluster0.jgb94.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
@@ -21,72 +21,72 @@ app.get('/', (req, res) => {
    res.send('Hello World!')
 })
 
-//params
+//param
 app.get('/:username/:password', (req, res) => {
    console.log(req.params)
-   res.send('Hello' + req.params.username + req.params.password)
-}) 
-
-//body
-app.get('/login', async (req,res) => {
-   const user= await client.db("anas").collection("anas0110").findOne(
-      {username: {$eq: req.body.username}}
-   )
-   //console.log(req.body)
-   //res.send('Login success' + req.body.username + req.body.password)
-   if(!user){
-      res.send('Username already exist')
-      return
-   }
-
-   console.log(req.body.password)
-   console.log(user.password)
-
-   //check password
-   const match = bcrypt.compareSync(req.body.password, user.password) // true
-
-   if(match){
-      res.send('Login success')
-   }else{
-      res.send('Login failed')
-   }
+   res.send('Hello World!' + req.params.username + req.params.password)
 })
 
 //body
-app.post('/register', async (req,res) => {
+app.get('/login', async (req, res) => {
+      //find if username exist?
+      const user = await client.db("anas").collection("anas0110").findOne(
+         { username:{ $eq:req.body.username }}
+      )
+      if (!user){
+         res.send('username not exist')
+         return
+       }
+
+       console.log(req.body.password)
+       console.log(user.password)
+
+      //compare a password
+      const match = bcrypt.compareSync(req.body.password, user.password);
+
+      if(match){
+         res.send('login success')
+      } else {
+         res.send('login failed')
+      }
+
+})
+
+//body
+app.post('/register', async (req, res) => {
    //find if username exist?
-   const user= await client.db("anas").collection("anas0110").findOne(
-      {username: {$eq: req.body.username}}
+   const user = await client.db("anas").collection("anas0110").findOne(
+      { username:{ $eq:req.body.username }}
    )
-
-   if(user){
-      res.send('Username already exist')
-      return
-   }
-
+      if (user){
+         res.send('username exist')
+         return
+       }
+   
    const hash = bcrypt.hashSync(req.body.password, saltRounds);
+
    client.db("anas").collection("anas0110").insertOne({
       "name": req.body.name,
       "username": req.body.username,
       "password": hash
    })
-   res.send('Register Success' + req.body.username + req.body.password)
+   res.send('Register Succesfully!' + req.body.username + req.body.password )
 })
 
 app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
+   console.log(Example app listening on port ${port})
 })
 
 async function run() {
    try {
-     // Connect the client to the server  (optional starting in v4.7)
+     // Connect the client to the server	(optional starting in v4.7)
      await client.connect();
      // Send a ping to confirm a successful connection
      await client.db("admin").command({ ping: 1 });
      console.log("Pinged your deployment. You successfully connected to MongoDB!");
    } finally {
      // Ensures that the client will close when you finish/error
-     //await client.close();
+  //   await client.close();
    }
  }
  run().catch(console.dir);
